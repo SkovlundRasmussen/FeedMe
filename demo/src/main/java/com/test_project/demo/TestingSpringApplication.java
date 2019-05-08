ff26232c1dc0de6459878144e16723383532f42b
Tommy Troest <tommytroest@hotmail.com>
2019-05-08 11:16:50 +0200

    hej

--- demo/src/main/java/com/test_project/demo/TestingSpringApplication.java
+++ demo/src/main/java/com/test_project/demo/TestingSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingSpringApplication {
 
+	// hej
 	public static void main(String[] args) {
 		SpringApplication.run(TestingSpringApplication.class, args);
 	}
